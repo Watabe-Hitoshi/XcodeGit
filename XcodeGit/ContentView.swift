380a4de822dc196f90117a87c19b68f870cbcea9
Hitoshi <hitoshiwtb1104@gmail.com>
2025-02-09 13:18:55 +0900

    2回目のコミット

--- XcodeGit/ContentView.swift
+++ XcodeGit/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("初めてのコミット")
+            // 2回目のコミット
+            // amendって何
         }
         .padding()
     }
