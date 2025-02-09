c9478fc80b017622a427ebcd98af35f982146f8d
Hitoshi <hitoshiwtb1104@gmail.com>
2025-02-09 12:01:13 +0900

    Initial Commit

---
+++ XcodeGit/XcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitApp.swift
+//  XcodeGit
+//
+//  Created by hitoshi on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
